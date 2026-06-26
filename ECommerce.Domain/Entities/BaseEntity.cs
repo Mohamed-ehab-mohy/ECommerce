7325e2027991df781c2e081ae1afba8e593dc0f2
Mohamed Ehab Mohy <mohamedehab92005@gmail.com>
2026-06-26 18:34:44 +0300

    feat: add IsDeleted property to BaseEntity for soft delete support

--- ECommerce.Domain/Entities/BaseEntity.cs
+++ ECommerce.Domain/Entities/BaseEntity.cs
@@ -5,4 +5,5 @@ public abstract class BaseEntity
     public Guid Id { get; protected set; }
     public DateTimeOffset CreatedAt { get; protected set; }
     public DateTimeOffset? UpdatedAt { get; protected set; }
+    public bool IsDeleted { get; protected set; }
 }
