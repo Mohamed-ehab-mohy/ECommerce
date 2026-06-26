5a65bacc2eeb93f86687e29984cef5fe8003a84b
Mohamed Ehab Mohy <mohamedehab92005@gmail.com>
2026-06-26 17:52:28 +0300

    feat: add BaseEntity abstract class in Domain layer

---
+++ ECommerce.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,8 @@
+namespace ECommerce.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; protected set; }
+    public DateTimeOffset CreatedAt { get; protected set; }
+    public DateTimeOffset? UpdatedAt { get; protected set; }
+}
