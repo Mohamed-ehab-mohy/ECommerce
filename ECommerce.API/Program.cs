bcaccc669ed0f2f20e1fb577592fc851bf1c1c18
Mohamed Ehab Mohy <mohamedehab92005@gmail.com>
2026-06-26 17:40:36 +0300

    feat: configure Program.cs with controllers and DI registration for all layers

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -1,3 +1,6 @@
+using ECommerce.Infrastructure;
+using ECommerce.UseCases;
+
 namespace ECommerce.API
 {
     public class Program
@@ -6,15 +9,16 @@ namespace ECommerce.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddControllers();
+            builder.Services.AddInfrastructure();
+            builder.Services.AddApplication();
             builder.Services.AddAuthorization();
 
             var app = builder.Build();
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-          
+            app.MapControllers();
 
             app.Run();
         }
