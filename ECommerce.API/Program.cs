81a61d9af0aa5b2dfea18b78b312c78601085714
Mohamed Ehab Mohy <mohamedehab92005@gmail.com>
2026-06-26 20:18:46 +0300

    feat: auto-apply migrations and seed data on startup in Development

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -1,11 +1,14 @@
 using ECommerce.Infrastructure;
+using ECommerce.Infrastructure.Data.DbContexts;
+using ECommerce.Infrastructure.Persistence.Seeding;
 using ECommerce.UseCases;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -16,11 +19,21 @@ namespace ECommerce.API
 
             var app = builder.Build();
 
+            if (app.Environment.IsDevelopment())
+            {
+                using var scope = app.Services.CreateScope();
+                var dbContext = scope.ServiceProvider.GetRequiredService<StoreDbContext>();
+                await dbContext.Database.MigrateAsync();
+
+                var seeder = scope.ServiceProvider.GetRequiredService<DatabaseSeeder>();
+                await seeder.SeedAllAsync();
+            }
+
             app.UseHttpsRedirection();
             app.UseAuthorization();
             app.MapControllers();
 
-            app.Run();
+            await app.RunAsync();
         }
     }
 }
