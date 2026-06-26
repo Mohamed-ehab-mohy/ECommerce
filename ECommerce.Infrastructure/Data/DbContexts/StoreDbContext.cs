f5f33efa1c47bc7fd84387a1411835ae01e3f170
Mohamed Ehab Mohy <mohamedehab92005@gmail.com>
2026-06-26 18:09:20 +0300

    feat: add StoreDbContext with auto-discovered entity configurations

---
+++ ECommerce.Infrastructure/Data/DbContexts/StoreDbContext.cs
@@ -0,0 +1,18 @@
+using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infrastructure.Data.DbContexts;
+
+public class StoreDbContext : DbContext
+{
+    public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductBrand> ProductBrands { get; set; }
+    public DbSet<ProductType> ProductTypes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreDbContext).Assembly);
+    }
+}
